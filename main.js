775bc8d503abd740d7ca4cd88a8421bd32f9f5de
Banstein <islambahnas1@gmail.com>
2022-02-14 22:21:37 +0200

    Add Html CSS JS RM Files

---
+++ main.js
@@ -0,0 +1,36 @@
+// Book Class: Represents a Book
+
+// UI Class: Handle UI Tasks
+
+    // Vanish in 3 seconds
+
+
+// Store Class: Handles Storage
+
+
+// Event: Display Books
+
+// Event: Add a Book
+  // Prevent actual submit
+
+  // Get form values
+
+
+  
+  // Validate
+    // Instatiate book
+
+    // Add Book to UI
+
+    // Add book to store
+
+    // Show success message
+
+    // Clear fields
+
+// Event: Remove a Book
+  // Remove book from UI
+
+  // Remove book from store
+
+  // Show success message
